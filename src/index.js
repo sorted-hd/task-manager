7fd9d6691524705602f91d72b09f58c8ddc83c4f
sorted-hd <hdubey71@gmail.com>
2022-02-21 00:53:40 +0530

    Testing v2

--- src/index.js
+++ src/index.js
@@ -9,7 +9,7 @@ const app = express();
 app.use(express.json());
 app.use(userRouter);
 app.use(taskRouter);
-console.log('Testing');
+
 app.listen(PORT, () => {
     console.log(`Server is up on port ${PORT}`);
 });
