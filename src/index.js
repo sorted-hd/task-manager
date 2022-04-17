9110546c3c4a45679e50a75d2e597b994c515a93
sorted-hd <hdubey71@gmail.com>
2022-04-17 16:09:49 +0530

    Updated Swagger

--- src/index.js
+++ src/index.js
@@ -3,8 +3,35 @@ require('./db/mongoose');
 const userRouter = require('./routes/user');
 const taskRouter = require('./routes/task');
 const { PORT } = require('./config/config');
+const swaggerUI = require('swagger-ui-express');
+const swaggerJsDoc = require('swagger-jsdoc');
+const swaggerConfig = require('./swagger.json');
+
+const options = {
+    definition: {
+        openapi: '3.0.0',
+        info: {
+            title: 'Task Manager API',
+            version: '1.0.0',
+            description: 'An Express Task Manager API'
+        },
+        servers: [
+            {
+                url: 'http://localhost:3000'
+            },
+            {
+                url: 'https://task-app-hd.herokuapp.com/'
+            }
+        ]
+    },
+    apis: ['./routes/*.js']
+};
+
+// const specs = swaggerJsDoc(swaggerConfig);
 
 const app = express();
+
+app.use('/api-docs', swaggerUI.serve, swaggerUI.setup(swaggerConfig));
 
 app.use(express.json());
 app.use(userRouter);
